acc292e3a41e10979e34e30e56ef7ab144149747
gk100799 <55948183+gk100799@users.noreply.github.com>
2020-10-27 17:08:40 +0530

    Updates

---
+++ precache-manifest.461c596bba533bf553e7a14ddb8db332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5915b471de8a5c55f219c3d3bc4dc44b",
+    "url": "/react-header/index.html"
+  },
+  {
+    "revision": "ee2c366060db3c257dcc",
+    "url": "/react-header/static/css/main.209f0285.chunk.css"
+  },
+  {
+    "revision": "28fee8fbfc81aac06e2c",
+    "url": "/react-header/static/js/2.7d7186f1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-header/static/js/2.7d7186f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2c366060db3c257dcc",
+    "url": "/react-header/static/js/main.cdd920e4.chunk.js"
+  },
+  {
+    "revision": "4ef43d52704d6831429a",
+    "url": "/react-header/static/js/runtime-main.82f4d5b6.js"
+  }
+]);
